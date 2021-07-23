3fbc301cbf7df7009754436915081362391d88b1
Tanush Banerjee <samihanbanerjee026@gmail.com>
2021-07-23 19:05:39 +0530

    Initial Commit!

---
+++ elpa/loop-20160813.1407/loop-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from loop.el  -*- no-byte-compile: t -*-
+(define-package "loop" "20160813.1407" "friendly imperative loop structures" 'nil :commit "e22807f83a0890dc8a904c51ee0742c34efccc6c" :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk") :keywords '("loop" "while" "for each" "break" "continue"))
---
+++ elpa/magit-section-20210702.822/magit-section-pkg.el
@@ -0,0 +1,13 @@
+(define-package "magit-section" "20210702.822" "Sections for read-only buffers"
+  '((emacs "25.1")
+    (dash "20210330"))
+  :commit "99c5468bd497a225ad047b2584854905c246bea7" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
